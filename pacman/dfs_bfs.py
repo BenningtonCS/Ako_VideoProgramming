c3335d36410cc0e9506f9b9aefc8638c3fa38f08
Ako Shergazy <akylaiyms@bennington.edu>
2021-03-17 16:52:36 -0400

    got rid of errors, but not working as intended

--- pacman/dfs_bfs.py
+++ pacman/dfs_bfs.py
@@ -1,92 +1,59 @@
-class Stack:
-     def __init__(self):
-         self.items = []
+def DepthFirstSearch(problem):
+    # emtpy container for visited nodes 
+    visited_nodes = []
+    # empty dictionary for transitions
+    transitions = {}
+    # stack
+    stack = Stack
+    stack.push(problem.getStartState())
+    while not stack.isEmpty(): 
+        current_node = stack.pop()
+        if problem.isGoalState(current_node):
+            return path(current_node, transitions)
+            visited_nodes.append(current_node)
 
-     def isEmpty(self):
-         return self.items == []
+        for i in problem.getSuccessors(current_node):
+            if not i in visited_nodes:
+                stack.push(i)
+                transitions.update({current_node:i})
 
-     def push(self, item):
-         self.items.append(item)
 
-     def pop(self):
-         return self.items.pop()
-
-     def peek(self):
-         return self.items[len(self.items)-1]
-
-     def size(self):
-         return len(self.items)
-
-stack = Stack
-visited_nodes1 = []
-dictionary1 = dict()
-tree1 = {"A": ["D", "C", "B"], "B": ["E"], "C": ["F"], "D": ["G"], "E": ["H"]}
-
-def DepthFirstSearch(goal, stack):
-    stack.push(tree1[0])
-    while stack is not 0:
-        stack = stack.pop()
-        if goal is stack:
-            break
-        else: 
-            visited_nodes1.append
-
-        for i in tree1:
-            if i is not visited_nodes1:
-                stack.push(tree1[i])
-                dictionary1.append({i}:{i + 1})
     backwards_path = []
-    current = goal
+    current_node = goal
 
-    while current is in dictionary1:
-        backwards_path.append(current)
-        current = dictionary1.pop()
+    while current in dictionary:
+        backwards_path.append(current_node)
+        current_node = dictionary.pop()
 
     forwards_path = backwards_path.reverse
     return forwards_path
 
 
-class Queue:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def enqueue(self, item):
-        self.items.insert(0,item)
-
-    def dequeue(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-
-queue = Queue
-visited_nodes2 = []
-dictionary2 = dict()
-tree2 = {"A": ["D", "C", "B"], "B": ["E"], "C": ["F"], "D": ["G"], "E": ["H"]}
-
 def BreadthFirtsSearch(goal, queue):
-    queue.push(tree2[0])
-    while queue is not 0:
-        queue = queue.dequeue()
-        if goal is queue:
-            break
-        else: 
-            visited_nodes2.append
+    # emtpy container for visited nodes 
+    visited_nodes = []
+    # empty dictionary for transitions
+    transitions = {}
+    # queue
+    queue = Queue
+    queue.enqueue(problem.getStartState())
+    while not queue.isEmpty():
+        current_node = queue.dequeue()
+        if problem.isGoalState(current_node):
+            return path(current_node, transitions)
+            visited_nodes.append(current_node)
         
-        for i in tree2:
-            if i is not visited_nodes2:
-                queue.enqueue(tree2[i])
-                dictionary2.append({i}:{i + 1})
-    backwards_path = []
-    current = goal
+        for i in problem.getSuccessors(current_node):
+            if not i in visited:
+                queue.enqueue(i)
+                transitions.update({current_node:i})
 
-    while current is in dictionary2:
-        backwards_path.append(current)
-        current = dictionary2.pop()
+    backwards_path = []
+    current_node = goal
+
+    while current_node in dictionary:
+        backwards_path.append(current_node)
+        current = dictionary.pop()
 
     forwards_path = backwards_path.reverse
     return forwards_path
