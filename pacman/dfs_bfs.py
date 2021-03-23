a1a86d04904de9fc7c4dc9afe003c1a4d0a9c747
Ako Shergazy <akylaiyms@bennington.edu>
2021-03-22 21:53:33 -0400

    ucs

--- pacman/dfs_bfs.py
+++ pacman/dfs_bfs.py
@@ -1,4 +1,5 @@
 import pacman
+import util
 
 def DepthFirstSearch(problem):
     # emtpy container for visited nodes 
@@ -6,29 +7,29 @@ def DepthFirstSearch(problem):
     # empty dictionary for transitions
     transitions = {}
     # stack
-    stack = Stack
-    stack.push(problem.getStartState())
-    while not stack.isEmpty(): 
-        current_node = stack.pop()
+    s = Stack()
+    s.push(problem.getStartState())
+    while not s.isEmpty(): 
+        current_node = s.pop()
         if problem.isGoalState(current_node):
-            return path(current_node, transitions)
+            return find_path(current_node, transitions)
             visited_nodes.append(current_node)
 
         for i in problem.getSuccessors(current_node):
             if not i in visited_nodes:
-                stack.push(i)
+                s.push(i)
                 transitions.update({current_node:i})
 
+    def find_path(goal, transition):
+        backwards_path = []
+        current_node = goal
 
-    backwards_path = []
-    current_node = goal
+        while current_node in transitions:
+            backwards_path.append(current_node)
+            current_node = transitions[current_node]
 
-    while current in dictionary:
-        backwards_path.append(current_node)
-        current_node = dictionary.pop()
-
-    forwards_path = backwards_path.reverse
-    return forwards_path
+        forwards_path = backwards_path.reverse
+        return forwards_path
 
 
 
@@ -38,26 +39,61 @@ def BreadthFirtsSearch(problem):
     # empty dictionary for transitions
     transitions = {}
     # queue
-    queue = Queue
-    queue.enqueue(problem.getStartState())
-    while not queue.isEmpty():
-        current_node = queue.dequeue()
+    q = Queue()
+    q.enqueue(problem.getStartState())
+    while not q.isEmpty():
+        current_node = q.dequeue()
         if problem.isGoalState(current_node):
-            return path(current_node, transitions)
+            return find_path(current_node, transitions)
             visited_nodes.append(current_node)
         
         for i in problem.getSuccessors(current_node):
-            if not i in visited:
-                queue.enqueue(i)
+            if not i in visited_nodes:
+                q.enqueue(i)
                 transitions.update({current_node:i})
 
+    def find_path(goal, transition):
 
-    backwards_path = []
-    current_node = goal
+        backwards_path = []
+        current_node = goal
 
-    while current_node in dictionary:
-        backwards_path.append(current_node)
-        current = dictionary.pop()
+        while current_node in transitions:
+            backwards_path.append(current_node)
+            current_node = transitions[current_node]
 
-    forwards_path = backwards_path.reverse
-    return forwards_path
+        forwards_path = backwards_path.reverse
+        return forwards_path
+
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    # emtpy container for visited nodes 
+    visited_nodes = []
+    # empty dictionary for transitions
+    transitions = {}
+    # priority queue
+    pq = PriorityQueue()
+    pq.push(problem.getStartState())
+    while not pq.isEmpty():
+        current_node = pq.pop()
+        if problem.isGoalState(current_node):
+            return find_path(current_node, transitions)
+            visited_nodes.append(current_node)
+        
+        for i in problem.getSuccessors(current_node):
+            if not i in visited_nodes:
+                pq.push(i)
+                transitions.update({current_node:i})
+
+    def find_path(goal, transition):
+
+        backwards_path = []
+        current_node = goal
+
+        while current_node in transitions:
+            backwards_path.append(current_node)
+            current_node = transitions[current_node]
+
+        forwards_path = backwards_path.reverse
+        return forwards_path
--- pacman/search.py
+++ pacman/search.py
@@ -1,3 +1,4 @@
+
 # search.py
 # ---------
 # Licensing Information:  You are free to use or extend these projects for
@@ -87,17 +88,102 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # emtpy container for visited nodes 
+    visited_nodes = []
+    # empty dictionary for transitions
+    transitions = {}
+    # stack
+    s = Stack()
+    s.push(problem.getStartState())
+    while not s.isEmpty(): 
+        current_node = s.pop()
+        if problem.isGoalState(current_node):
+            return find_path(current_node, transitions)
+            visited_nodes.append(current_node)
+
+        for i in problem.getSuccessors(current_node):
+            if not i in visited_nodes:
+                s.push(i)
+                transitions.update({current_node:i})
+
+    def find_path(goal, transition):
+        backwards_path = []
+        current_node = goal
+
+        while current_node in transitions:
+            backwards_path.append(current_node)
+            current_node = transitions[current_node]
+
+        forwards_path = backwards_path.reverse
+        return forwards_path
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # emtpy container for visited nodes 
+    visited_nodes = []
+    # empty dictionary for transitions
+    transitions = {}
+    # queue
+    q = Queue()
+    q.push(problem.getStartState())
+    while not q.isEmpty():
+        current_node = q.pop()
+        if problem.isGoalState(current_node):
+            return find_path(current_node, transitions)
+            visited_nodes.append(current_node)
+        
+        for i in problem.getSuccessors(current_node):
+            if not i in visited_nodes:
+                q.push(i)
+                transitions.update({current_node:i})
+
+    def find_path(goal, transition):
+
+        backwards_path = []
+        current_node = goal
+
+        while current_node in transitions:
+            backwards_path.append(current_node)
+            current_node = transitions[current_node]
+
+        forwards_path = backwards_path.reverse
+        return forwards_path
+
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # emtpy container for visited nodes 
+    visited_nodes = []
+    # empty dictionary for transitions
+    transitions = {}
+    # priority queue
+    pq = PriorityQueue()
+    pq.push(problem.getStartState())
+    while not pq.isEmpty():
+        current_node = pq.pop()
+        if problem.isGoalState(current_node):
+            return find_path(current_node, transitions)
+            visited_nodes.append(current_node)
+        
+        for i in problem.getSuccessors(current_node):
+            if not i in visited_nodes:
+                pq.push(i)
+                transitions.update({current_node:i})
+
+    def find_path(goal, transition):
+
+        backwards_path = []
+        current_node = goal
+
+        while current_node in transitions:
+            backwards_path.append(current_node)
+            current_node = transitions[current_node]
+
+        forwards_path = backwards_path.reverse
+        return forwards_path
 
 def nullHeuristic(state, problem=None):
     """
