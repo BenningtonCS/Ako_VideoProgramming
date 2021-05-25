99a22c3f0ae590222d829f15dc17e6a1783592e8
Ako Shergazy <akylaiyms@bennington.edu>
2021-05-25 17:33:16 -0400

    vr project

---
+++ final_project/GVR Project/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject camera3;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("RKey"))
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("LKey"))
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            camera3.SetActive(false);
+        }
+
+        if(Input.GetButtonDown("UKey"))
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(true);
+        }
+    }
+}
